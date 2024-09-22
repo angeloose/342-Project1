ad102b8f27e5af62ecaefdf4795295d63f48196c
Angelo <angelocervanaa@gmail.com>
2024-09-21 19:51:21 -0700

    done with C coding

--- sorting.h
+++ sorting.h
@@ -1,9 +1,12 @@
 #ifndef SORTING_H       // include guard, prevents multiple inclusions of header files in program
 #define SORTING_H
 
-void quick_sort(int *a, int left, int right);   // quick sort an array a with n elements in C
+int partition(int*, int left, int right);           // quick sort helper func
 
+void quick_sort(int *a, int left, int right);       // quick sort an array a with n elements in C
 
-void merge_sort(int *a, int n);   // merge sort an array a with n elements in C
+void merge(int *a, int left, int mid, int right);   // merge sort helper func
+
+void merge_sort(int *a, int left, int right);                     // merge sort an array a with n elements in C
 
 #endif
